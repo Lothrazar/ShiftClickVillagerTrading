6a31563731dddeea4a50ca46c275dc6495bd500c
Sam <lothrazar@hotmail.com>
2026-08-25 20:27:59 -0700

    porting 26.1

--- src/main/java/com/lothrazar/shiftclickvillagertrading/ModRegistry.java
+++ src/main/java/com/lothrazar/shiftclickvillagertrading/ModRegistry.java
@@ -8,10 +8,10 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModRegistry {
 
-  public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(BuiltInRegistries.ITEM, ModMain.MODID);
-  public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(BuiltInRegistries.BLOCK, ModMain.MODID);
+  public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(ModMain.MODID);
+  public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(ModMain.MODID);
   public static final DeferredRegister<BlockEntityType<?>> TILE_ENTITIES = DeferredRegister.create(BuiltInRegistries.BLOCK_ENTITY_TYPE, ModMain.MODID);
-  //  public static final DeferredHolder<Item, Item> OVERWORLD_KEY = ITEMS.register("whatever", () -> new Item(new Item.Properties()));
-  //  public static final DeferredHolder<Block, Block> STUFF_BLOCK = BLOCKS.register("stuff", () -> new Block(Block.Properties.of()));
-  //  public static final DeferredHolder<Item, Item> STUFF_ITEM = ITEMS.register("stuff", () -> new BlockItem(STUFF_BLOCK.get(), new Item.Properties()));
+  //  public static final DeferredHolder<Item, Item> OVERWORLD_KEY = ITEMS.registerItem("whatever", props -> new Item(props));
+  //  public static final DeferredHolder<Block, Block> STUFF_BLOCK = BLOCKS.registerBlock("stuff", props -> new Block(props));
+  //  public static final DeferredHolder<Item, Item> STUFF_ITEM = ITEMS.registerItem("stuff", props -> new BlockItem(STUFF_BLOCK.get(), props));
 }
